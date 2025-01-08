5f11f5749e38e0bc15dba6db1fa5f910ee6d797e
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-08 02:44:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Importa el paquete CORS
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
+app.use(cors());
 app.use(express.json());
 
-// Habilitar CORS para permitir solicitudes desde cualquier origen
-app.use(cors());
-
-// Habilitar CORS para permitir solicitudes desde Netlify
-app.use(cors());
-
-// Connect to MongoDB using Railway-provided URL
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
-    console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');
 }).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
+  console.error('Error connecting to MongoDB:', err);
 });
 
-// Routes
 app.use('/clientes', require('./routes/clientes'));
 app.use('/pagos', require('./routes/pagos'));
-app.use('/asistencia', require('./routes/asistencia'));
 app.use('/reportes', require('./routes/reportes'));
+app.use('/asistencia', require('./routes/asistencia'));
 app.use('/configuracion', require('./routes/configuracion'));
 
-// Start server
 app.listen(port, () => {
-    console.log(`Backend running at http://localhost:${port}`);
+  console.log(`Backend running at http://localhost:${port}`);
 });
+
