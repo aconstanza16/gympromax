3057bb4cf9d762d03e7f92ef00b02fd5903b254c
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-08 02:04:52 -0400

    Update App.js

--- gym-frontend/src/App.js
+++ gym-frontend/src/App.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Typography, Grid, Paper } from '@mui/material';
 
-const App = () => {
+const App = 'https://gympromax-production.up.railway.app';
+ () => {
     return (
         <Grid container spacing={3}>
             <Grid item xs={12} md={6}>
