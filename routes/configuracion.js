eb9013502780c8b27f7650e772809291eeb485f6
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-08 02:09:38 -0400

    Update configuracion.js

--- routes/configuracion.js
+++ routes/configuracion.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const API_URL = 'https://gympromax-production.up.railway.app';
+export default API_URL;
 // Ruta de prueba para configuración
 router.get('/', (req, res) => {
   res.json({ message: 'Ruta de configuración funcionando' });
