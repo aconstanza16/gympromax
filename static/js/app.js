2157c074ef632a6a9c492ef1fe90b6d330013ba5
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-07 22:44:41 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,22 @@
 
-console.log("Gym Management System is running.");
+function manageMembershipPlans() {
+    alert('Función: Gestionar Planes de Membresía');
+}
+
+function configureAttendanceDevices() {
+    alert('Función: Configurar Controles de Asistencia');
+}
+
+function manageUsers() {
+    alert('Función: Gestión de Usuarios');
+}
+
+function dailyCashReport() {
+    alert('Función: Cuadre de Caja (Hoy)');
+}
+
+function customDateReport() {
+    alert('Función: Reporte por Fecha');
+}
+
+console.log("Gym Management System with additional modules loaded.");
