4d0fee426357483a852bd6598fcc6a1466c6d2f6
aconstanza16 <anthonyconstanza1@gmail.com>
2025-01-08 13:22:25 -0400

    Add files via upload

---
+++ gym-backend/models/User.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    pagos: [{
+        monto: Number,
+        fecha: { type: Date, default: Date.now },
+    }],
+});
+
+module.exports = mongoose.model('User', UserSchema);
